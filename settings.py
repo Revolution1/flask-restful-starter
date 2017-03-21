7fa569541a3055613dc233b81df5b525a976d1cb
revol.cai <revol.cai@daocloud.io>
2017-03-21 17:53:43 +0800

    update

--- settings.py
+++ settings.py
@@ -9,8 +9,8 @@ SECRET_KEY = os.getenv('SECRET_KEY') or 'flask_restful_starter'
 SOURCE_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 DATA_DIR = os.path.join(SOURCE_ROOT, 'data')
-if not os.path.exists(DATA_DIR):
-    os.mkdir(DATA_DIR)
+# if not os.path.exists(DATA_DIR):
+#     os.mkdir(DATA_DIR)
 
 
 def in_data_dir(*path):
@@ -23,11 +23,13 @@ PROD = os.getenv('PROD') or False
 GUNICORN_WORKERS = os.getenv('GUNICORN_WORKERS')
 
 LOG_LEVEL = logging.INFO if PROD else logging.DEBUG
-
+LOG_FORMAT = '%(asctime)s (%(process)d/%(threadName)s) %(name)s %(levelname)s - %(message)s'
 
 def setup_logging(level=None):
     level = level or LOG_LEVEL
     console_handler = logging.StreamHandler(sys.stderr)
+    formatter = logging.Formatter(LOG_FORMAT)
+    console_handler.setFormatter(formatter)
     root_logger = logging.getLogger()
     root_logger.addHandler(console_handler)
     root_logger.setLevel(level)
